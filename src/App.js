79b2f66eab531eeee59ccc29a9b16ab2dcb6b3ee
Merik <merikreyimov@gmail.com>
2026-06-12 17:48:26 +0300

    Tailwind added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to My React App!</h1>
+      <h1 className=" text-red-500 text-underlined">
+        Welcome to My React App!
+      </h1>
     </div>
   );
 }
